cbaeabb1f58e53f85e4e7840e07e9f04550b93ee
Harin-Mabao <mixharinmabao@gmail.com>
2024-06-29 04:43:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let a =typeof 'x';
+let b =typeof x;
+let c =typeof ``;
+let d =typeof Infinity;
+let e =typeof(100 / 0);
+let f =typeof(1 + '3');
+let g =typeof(1 * 'a');
+let h =typeof null;
+
+console.log("typeof 'x' is :", a)
+console.log("typeof x is ", b)
+console.log("typeof `` is ", c)
+console.log("typeof Infinity is ", d)
+console.log("typeof(100 / 0) is ", e)
+console.log("typeof(1 + '3') is ", f)
+console.log("typeof(1 * 'a' is ", g)
+console.log("typeof null is ", h)
